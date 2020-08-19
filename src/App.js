a4f54ab62788e6d6658db712073e9c214aa062a6
doakcovington <doakcovington@protonmail.com>
2020-08-18 18:34:41 -0700

    Added ListRecords component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 
 //components
 import InputRecord from "./components/InputRecord"
+import ListRecords from "./components/ListRecords"
+
 
 function App() {
   return  (
     <Fragment>
       <div className="container">
         <InputRecord />
+        <ListRecords />
       </div>
     </Fragment>
   );
